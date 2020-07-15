eec1c5e81677a2faa69d629e6ed514a4175d880a
Mariusz <mariopl@poczta.onet.eu>
2020-07-15 22:46:50 +0200

    add CSV.py

---
+++ CSV.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+
+in1 = sys.argv[1]
+in2 = sys.argv[2]
+out = sys.argv[3]
+
+df1 = pd.read_csv(in1)
+df2 = pd.read_csv(in2)
+pd.merge(df1, df2, 'outer').to_csv(out, index=False)
